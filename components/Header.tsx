a2b7ec461c248f939cf8e1e1e3ad20993e19c0bb
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-08-10 12:33:19 +0300

    create global styles

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import React, { PropsWithChildren } from 'react'
+import cs from '../styles/Header.module.sass'
+
+function Header() {
+	return (
+	<header className={`${cs.header} ${cs.header_close}`}>
+    <div className={cs.header__logo}></div>
+      <nav className={cs.header__menu }>
+        <ul className={cs.header__list}>
+          <li className={cs.header__listItem}><a href="#">Home</a></li>
+          <li className={cs.header__listItem}><a href="#">About</a></li>
+          <li className={cs.header__listItem}><a href="#">Contact</a></li>
+          <li className={cs.header__listItem}><a href="#">Blog</a></li>
+          <li className={cs.header__listItem}><a href="#">Careers</a></li>
+        </ul>
+      </nav>
+      <a href="#" className="invite-button">
+        Request Invite
+      </a>
+      <button
+      title="Navigation close and open"
+      className={cs.header__hamburgerButton}></button>
+  </header>
+	)
+}
+
+export default Header
