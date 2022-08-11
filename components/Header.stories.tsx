ee552da04c0e4c5370b533134c6d8d6dc4af35b2
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-08-11 11:02:02 +0300

    storybook setup finished

---
+++ components/Header.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Header from './Header';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Primary = Template.bind({});
